01523d0301afd3f83a4991bbd2c2bb6bdd9385d0
prap4source <pradeep.gopanapalli@gmail.com>
2018-10-10 14:19:41 -0700

    Update memory.c
    Update function description

--- src_c/memory.c
+++ src_c/memory.c
@@ -5,7 +5,7 @@
  *      Author: Pradeep Gopanapalli
  */
 #include "common.h"
-
+/* Allocate aligned  memory ,Use a extra space at front of memory to store offset */
 void *amalloc(int size, int alignment) {
     void *mem, *ptr;
     int offset = (alignment - 1) + sizeof(char); /*sizeof(char) for storing information */
@@ -16,7 +16,8 @@ void *amalloc(int size, int alignment) {
 	*(char *)(ptr - sizeof(char)) = (ptr - mem);
 	return ptr;
 }
-
+/* Free aligned  memory ,this routine will be ptr to data memory so we need to find 
+   actual memory starting */
 void afree(void *ptr) {
 	int  off = *(char *)(ptr - sizeof(char));
 	printf ("%p \n",ptr-off);
