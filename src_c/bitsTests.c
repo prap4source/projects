7111cc9bd21a216469bfc9aa4a9558d543eae30a
prap4source <pradeep.gopanapalli@gmail.com>
2018-10-11 16:50:16 -0700

    Update bitsTests.c
    Update description

--- src_c/bitsTests.c
+++ src_c/bitsTests.c
@@ -14,7 +14,7 @@ typedef struct strfunc {
 }sfunc;
 		    
 /* (INT_QUEST)Set bits between two indexes You are given two 32-bit numbers, N and M, and two bit positions, i and j 
- *  Write a method to set all bits between i and j in N equal to M (e g , M becomes a substring of N located at i and starting at j)   
+ *  Write a method to put/set all bits between i and j in N equal to M (e g , M becomes a substring of N located at i and starting at j)   
     EX:N = 10000000000, M = 10101, i = 2, j = 6 Output: N = 10001010100
     This code operates by clearing all bits in N between position i and j, and then OR to put M in there */
 int updateBits(int N, int M, int i, int j) {
