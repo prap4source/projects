6de57a4157e33a0b7f1f6c2a0bd5e6012a7a5f99
prap4source <pradeep.gopanapalli@gmail.com>
2018-10-14 21:12:19 -0700

    Update bitsops.h
    Add my_test_and_set_bit atomic functions

--- include_c/bitsops.h
+++ include_c/bitsops.h
@@ -64,12 +64,23 @@ static inline int my_test_and_change_bit(int nr, unsigned long *addr) {
     old = *p;
     *p ^= (bitmask);
     raw_local_irq_restore(flags);
-
     return (old & bitmask) != 0;
-
-
-
 }
+
+static inline int my_test_and_set_bit(int nr , unsigned long *addr) {
+	ulong *p = ((ulong *)addr + nr / BITS_PER_LONG);
+	ulong bitmask = 1 << (nr % BITS_PER_LONG);
+	ulong old;
+	ulong flags;
+
+	raw_local_irq_save(flags);
+	old = *p;
+	*p |= mask;
+	raw_local_irq_restore(flags);
+	
+	return (old & mask) != 0;
+}
+	
 
 static inline void  my_set_bit(int nr, unsigned long *addr) {
     unsigned long *p = ((unsigned long *)addr + (nr / BITS_PER_LONG));
