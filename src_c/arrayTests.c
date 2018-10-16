792b90c31420dad29d8d784de27ac0385e68405b
prap4source <pradeep.gopanapalli@gmail.com>
2018-10-16 06:14:20 -0700

    Code fibonacci numbers using Dynammic programming

--- src_c/arrayTests.c
+++ src_c/arrayTests.c
@@ -53,3 +53,47 @@ int findMinSlideSize(int arr[], int m, int swSize) {
      
      return (min_size);
 }
+
+/* Find fibannoci series using Memorization method ,Dynammic programming method 1 
+ Memoization (Top Down): The memoized program for a problem is similar to the recursive 
+ version with a small modification that it looks into a lookup table before computing 
+ solutions. We initialize a lookup array with all initial values as NIL. Whenever we 
+ need the solution to a subproblem, we first look into the lookup table. If the 
+ precomputed value is there then we return that value, otherwise, 
+ we calculate the value and put the result in the lookup table so that it can be reused later.*/
+int f[MAX] = -1;
+int fibN(int n) {
+    if (f[n] == -1) {
+        if (n <= 1)
+           f[n] = n;
+        else
+            f[n] = fibN(n-1) + fibN(n-2);
+    }
+    
+    return f[n];
+}
+int findfibN(int n) {
+    int val;
+    if (n >= MAX) {
+        printf("n:%d >= MAX:%d\n", n, MAX);
+        return 1;
+    }
+	for (int i =0;i<=n;i++)
+	    f[i] = -1;
+	val = fibN(n);
+	printf("n:%d fib:%d \n", n, val);
+	return 0;
+}
+	
+
+/* Find fibannoci series using tabulation method ,Dynammic programming method 2
+(Bottom Up): The tabulated program for a given problem builds a table in bottom up fashion and 
+returns the last entry from table. For example, for the same Fibonacci number, we first calculate 
+fib(0) then fib(1) then fib(2) then fib(3) and so on. So literally, we are building the solutions of subproblems bottom-up. */
+int findFibN(int n) {
+	int f[n+1];
+	f[0] = 0;
+	f[1] =1;
+	for (int i =2;i<=n;i++)
+	    f[i] = f[i-1] + f[i-2];
+}
