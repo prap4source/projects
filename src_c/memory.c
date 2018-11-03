d0ef5b331a6f59ecae5dc29fd6dce1736af57820
prap4source <pradeep.gopanapalli@gmail.com>
2018-11-02 20:57:01 -0700

    Padding memory

--- src_c/memory.c
+++ src_c/memory.c
@@ -5,6 +5,29 @@
  *      Author: Pradeep Gopanapalli
  */
 #include "common.h"
+/* Write a function that pads a sequence of bytes to a multiple of a given block length.
+Requirements:
+* The value of each added byte should be the total number of added bytes.
+  (N bytes, each of value N are added.)
+* If the original sequence is a multiple of N bytes, an extra block of N bytes is added.
+
+Example (for block length 16)
+* 12 bytes, must be padded to 16 bytes, 4 bytes padding, 0x04 in each byte padded
+* 24 bytes, must be padded to 32 bytes, 8 bytes padding, 0x08
+* 31 bytes, must be padded to 32 bytes, 1 byte padding, 0x01
+* 38 bytes, block length is a multiple of 3 bytes, must be padded to 39 bytes 1 <0x01
+* 36 bytes, block length is a multiple of 3 bytes, must be padded to 39 bytes 3 <0x03
+/* size is size of block ,padto is number of bytes to pad 
+padto = (size < block) ? (block -size) : (block - size % block);
+func_padto(bytes,size,padto)
+char *func_padto(char *bytes, int size, int padto) {
+    int val = padto;
+    char *newBytes = realloc(bytes, size + padto);
+    for (int i =0 ;i <padto; i++)
+        *(char *)(newBytes + size + i) = val;
+    return newBytes; /*Padding to padto */
+}
+
 /* Allocate aligned  memory ,Use a extra space at front of memory to store offset */
 void *amalloc(int size, int alignment) {
     void *mem, *ptr;
