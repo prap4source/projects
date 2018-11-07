6ec163fc0413f69d6e4403a5f8281c58bbad92bb
prap4source <pradeep.gopanapalli@gmail.com>
2018-11-07 15:16:37 -0800

    Implement rw_* locks using mutex and condition variable

--- src_c/implementation.c
+++ src_c/implementation.c
@@ -5,11 +5,12 @@
  */
 #include "common.h"
 #include "bitsops.h"
+#include <pthread.h>
 /* cmpxchg 
  * if (*p == OLD)
- *      return false;
- * *p = NEW
- * return true;
+ *      *p = NEW
+ *      return true;
+ * return false;
  */
 #define cmpxchg(P, O, N) __sync_val_compare_and_swap(P, O, N)
 /* Compile read-write barrier */
@@ -35,4 +36,64 @@ void spin_lock(spinlock *lock) {
 void spin_unlock(spinlock *lock) {
 	barrier();
 	*lock = 0;
+}
+/* Implement rw locks of different types
+rw_rlock(...) { ... }; <- as many readers but no writers
+rw_wlock(...) { ... }; <- one writer ,no readers
+rw_runlock(...) { ... };
+rw_wunlock(...) { ... };
+*/
+struct rwlock
+{
+    pthread_mutex_t mtx;
+    int readers;
+    int writers;
+    int count; /* Only for writer */
+    pthread_cond_t  read_cv;
+    pthread_cond_t write_cv;
+};
+void rw_wlock(struct rwlock *lock) {
+    /* Hold mutex lock */
+    /* check for count, if count != 0 wait and wakeup*/
+        
+    pthread_mutex_lock(&lock->mtx);
+    while((lock->count != 0) || (lock->readers != 0)) {
+              lock->writers++;
+              pthread_cond_wait(&lock->write_cv, &lock->mtx);        
+    }
+    lock->writers--; 
+    lock->count++;
+    pthread_mutex_unlock(&lock->mtx);
+}
+
+void rw_wunlock(struct rwlock *lock) {
+    pthread_mutex_lock(&lock->mtx);
+    lock->count--;
+    if (lock->writers) {
+        pthread_mutex_unlock(&lock->mtx);
+        pthread_cond_signal(&lock->write_cv);
+    } else {
+        pthread_mutex_unlock(&lock->mtx);
+        pthread_cond_broadcast(&lock->read_cv);
+    }
+}
+
+void rw_rlock(struct rwlock *lock) {
+    /* Hold mutex lock */
+    /* check for count, if count != 0 wait and wakeup*/
+    pthread_mutex_lock(&lock->mtx);
+    while((lock->count != 0))
+        pthread_cond_wait(&lock->read_cv, &lock->mtx);
+    lock->readers++;
+    pthread_mutex_unlock(&lock->mtx);
+}
+
+void rw_runlock(struct rwlock *lock) {
+    pthread_mutex_lock(&lock->mtx);
+    lock->readers--;
+    pthread_mutex_unlock(&lock->mtx);
+    if (lock->writers)
+        pthread_cond_signal(&lock->write_cv);
+    else
+        pthread_cond_broadcast(&lock->read_cv);
 }
