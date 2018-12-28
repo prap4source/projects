8e28b00ae387618bc112e4b91fd5f91762f4c3e6
prap4source <pradeep.gopanapalli@gmail.com>
2018-12-27 16:19:33 -0800

    Changes

--- src_c/memory.c
+++ src_c/memory.c
@@ -33,17 +33,17 @@ void *amalloc(int size, int alignment) {
     void *mem, *ptr;
     int offset = (alignment - 1) + sizeof(char); /*sizeof(char) for storing information */
 
-	mem = malloc (size + offset );
-	printf("%p \n",mem);
-	ptr=(void**) (((size_t)(mem) + offset) & ~(alignment -1));
-	*(char *)(ptr - sizeof(char)) = (ptr - mem);
-	return ptr;
+    mem = malloc (size + offset );
+    printf("%p \n",mem);
+    ptr=(void**) (((size_t)(mem) + offset) & ~(alignment -1));
+    *(char *)(ptr - sizeof(char)) = (ptr - mem);
+    return ptr;
 }
+
 /* Free aligned  memory ,this routine will be ptr to data memory so we need to find 
    actual memory starting */
 void afree(void *ptr) {
 	int  off = *(char *)(ptr - sizeof(char));
 	printf ("%p \n",ptr-off);
 	free(ptr - off);
-
 }
